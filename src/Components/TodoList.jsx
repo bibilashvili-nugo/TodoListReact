08371e45fd1c226c461707ca76ac27835cfab051
Nugo Bibilashvili <94049041+bibilashvili-nugo@users.noreply.github.com>
2024-04-24 05:25:29 -0700

    update

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,26 +1,52 @@
 import TodoDelete from "./TodoDelete";
 import TodoFinish from "./TodoFinish";
-import "./TodoList.css";
 
-const TodoList = ({ todos, todoList, setTodoList, setChecked, checked }) => {
-  return todos.map((item) => (
-    <li key={item.id} className="list">
-      {checked ? (
-        <span className="line">{item.text}</span>
-      ) : (
-        <span>{item.text}</span>
-      )}
+const TodoList = ({ todos, todoList, setTodoList }) => {
+  const handleToggleCompletion = (itemId) => {
+    const updatedTodoList = todoList.map((item) =>
+      item.id === itemId ? { ...item, checked: !item.checked } : item
+    );
+    setTodoList(updatedTodoList);
+  };
 
-      <div className="list-components">
-        <TodoFinish setChecked={setChecked} checked={checked} />
-        <TodoDelete
-          itemId={item.id}
-          todoList={todoList}
-          setTodoList={setTodoList}
-        />
-      </div>
-    </li>
-  ));
+  return (
+    <ul style={{ listStyle: "none", padding: 0 }}>
+      {todos.map((item) => (
+        <li
+          key={item.id}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "10px",
+            borderBottom: "1px solid #ccc",
+          }}
+        >
+          <span
+            style={{
+              flexGrow: 1,
+              marginRight: "10px",
+              textDecoration: item.checked ? "line-through" : "none",
+            }}
+          >
+            {item.text}
+          </span>
+
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <TodoFinish
+              setChecked={() => handleToggleCompletion(item.id)}
+              checked={item.checked}
+            />
+            <TodoDelete
+              itemId={item.id}
+              todoList={todoList}
+              setTodoList={setTodoList}
+            />
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
